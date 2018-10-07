f55febcdd817e9273f63533e3f22e11ec68c0a62
andrew audrain <audrain@hoare7.cs.umsl.edu>
2018-10-06 20:15:41 -0500

    added some test functionality to user.c to check with Makefile.

---
+++ user.c
@@ -0,0 +1,11 @@
+/* Name: Andrew Audrain */
+/* File: user.c  |  Executable (after make): user */
+/* Date created: 10/06/18 */
+
+#include <stdio.h>
+
+int main ( int argc, char *argv[] ) {
+	printf ( "Hello, from user.\n" );
+
+	return 0;
+}
