e40b4758f8eff30741e0d58da878a3703d7637cd
andrew audrain <audrain@hoare7.cs.umsl.edu>
2018-10-10 01:31:48 -0500

    Implemented shared memory for user to access.

--- user.c
+++ user.c
@@ -3,10 +3,55 @@
 /* Date created: 10/06/18 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
 int main ( int argc, char *argv[] ) {
+
+	/* Variables for shared memory */
+	int clockBuffer[2];
+	key_t key1 = 1993;
+	int *shmClock;
+	int shmClockid;
+
+	int msgBuffer[3];
+	key_t key2 = 1994;
+	int *shmMsg;
+	int shmMsgid;
+
+	/* Accessing shared memory allocated in oss */
+	if ( ( shmClockid = shmget ( key1, sizeof ( clockBuffer), 0666 ) ) < 0 ) {
+		perror ( "Clock shmget" );
+		exit ( 1 );
+	} 
+	
+	if ( ( shmClock = ( int *) shmat ( shmClockid, NULL, 0 ) ) < 0 ) {
+		perror ( "Clock shmat" );
+		exit ( 1 );
+	}
+
+	if ( ( shmMsgid = shmget ( key2, sizeof ( msgBuffer ), 0666 ) ) < 0 ) {
+		perror ( "Message shmget" );
+		exit ( 1 );
+	}
+
+	if ( ( shmMsg = ( int *) shmat ( shmMsgid, NULL, 0 ) ) < 0 ) {
+		perror ( "Message shmat" );
+		exit ( 1 );
+	}
+
+	//shmClock[0]++;
+	//shmClock[1]++;
+	//shmMsg[0] = getpid();
+	//shmMsg[1] = shmClock[0];
+	//shmMsg[2] = shmClock[1];
 	
 	printf ( "Child -- pid %d from parent pid %d\n", getpid(), getppid() );
+	// printf ( "Child: %d terminated at %d.%d\n", shmMsg[0], shmMsg[1], shmMsg[2] );
 
 	return 0;
 }
