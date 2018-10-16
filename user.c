75211644c35a0282c659687a5800d518a2e9e425
andrew audrain <audrain@hoare7.cs.umsl.edu>
2018-10-15 22:02:40 -0500

    Implemented named semaphore for critical section control.

--- user.c
+++ user.c
@@ -10,51 +10,89 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <signal.h>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <time.h>
+#include <stdbool.h>
+
+#define SEM_NAME "/semaphore"
 
 int main ( int argc, char *argv[] ) {
+
+	/* Access the named semaphore created in oss */
+	sem_t *sem;
+
+	if ( ( sem  = sem_open ( SEM_NAME, O_CREAT, 0777, 1 ) ) == SEM_FAILED ) {
+		perror ( "sem_open" );
+		exit ( -1 );
+	}
 
 	/* Variables for shared memory */
 	int clockBuffer[2];
 	key_t key1 = 1993;
 	int *shmClock;
-	int shmClockid;
+	int shmClockID;
 
 	int msgBuffer[3];
 	key_t key2 = 1994;
 	int *shmMsg;
-	int shmMsgid;
+	int shmMsgID;
 
 	/* Accessing shared memory allocated in oss */
-	if ( ( shmClockid = shmget ( key1, sizeof ( clockBuffer), 0666 ) ) < 0 ) {
+	if ( ( shmClockID = shmget ( key1, sizeof ( clockBuffer), 0666 ) ) < 0 ) {
 		perror ( "Clock shmget" );
 		exit ( 1 );
 	} 
 	
-	if ( ( shmClock = ( int *) shmat ( shmClockid, NULL, 0 ) ) < 0 ) {
+	if ( ( shmClock = ( int *) shmat ( shmClockID, NULL, 0 ) ) < 0 ) {
 		perror ( "Clock shmat" );
 		exit ( 1 );
 	}
 
-	if ( ( shmMsgid = shmget ( key2, sizeof ( msgBuffer ), 0666 ) ) < 0 ) {
+	if ( ( shmMsgID = shmget ( key2, sizeof ( msgBuffer ), 0666 ) ) < 0 ) {
 		perror ( "Message shmget" );
 		exit ( 1 );
 	}
 
-	if ( ( shmMsg = ( int *) shmat ( shmMsgid, NULL, 0 ) ) < 0 ) {
+	if ( ( shmMsg = ( int *) shmat ( shmMsgID, NULL, 0 ) ) < 0 ) {
 		perror ( "Message shmat" );
 		exit ( 1 );
 	}
 
-	//shmClock[0]++;
-	//shmClock[1]++;
-	//shmMsg[0] = getpid();
-	//shmMsg[1] = shmClock[0];
-	//shmMsg[2] = shmClock[1];
+	/* Starts by reading the simulated time clock */
+	int seconds = shmClock[0];
+	int nano = shmClock[1];
+
+	srand ( time ( 0 ) );
+
+	int num = ( rand() % ( 1000000 - 1 + 1 ) ) + 1;
+	nano += num;
+	while ( nano > 1000000 ) {
+		seconds++;
+		nano = nano - 1000000;
+	}
+
+	bool stillAlive = true;
 	
-	sleep ( 10 );	
-	
-	printf ( "Child -- pid %d from parent pid %d\n", getpid(), getppid() );
-	// printf ( "Child: %d terminated at %d.%d\n", shmMsg[0], shmMsg[1], shmMsg[2] );
+	/* Critical Section */
+	do {
+		//printf ( "Child %d is attempting to enter the CS.\n", getpid() );
+		sem_wait ( sem );
+		//printf ( "Child %d has entered the CS.\n", getpid() );
+		if ( ( seconds + nano ) >= ( shmClock[0] + shmClock[1] ) && shmMsg[0] == 0 ) {
+			shmMsg[0] = getpid();
+			shmMsg[1] = seconds;
+			shmMsg[2] = nano;
+			
+			stillAlive = false;
+		}
+		/*else {
+			printf ( "Child %d left the CS without being able to terminate.\n", getpid() ); 
+		} */		
+		sem_post ( sem );
+	} while ( stillAlive );
+		
+	//printf ( "Child %d is able to terminate.\n", getpid() );	
 
 	return 0;
 }
